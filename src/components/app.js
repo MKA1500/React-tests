c340fdd72c495665ec8e48f61543cd627e3c7e6a
MKA1500 <maciejkabsch@gmail.com>
2017-07-16 12:07:08 +0200

    ...

---
+++ src/components/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class App extends React.Component {
+    render() {
+        return {
+            <div><h1>React Test</h1></div>
+        };
+    }
+}
