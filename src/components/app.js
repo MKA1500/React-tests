8918ed71b7f707a1df8387bf272f22e8e7602b6b
MKA1500 <maciejkabsch@gmail.com>
2017-07-16 20:52:21 +0200

    Compiled successfully

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import TodosList from './todos-list';
+
+const todos = [
+    {
+        task: 'do shopping',
+        isCompleted: false
+    },
+    {
+        task: 'eat dinner',
+        isCompleted: true
+    }
+];
 
 
 export default class App extends React.Component {
     render() {
-        return {
-            <div><h1>React Test</h1></div>
-        };
+        return (
+            <div>
+                <h1>React Test</h1>
+                <TodosList />
+            </div>
+    );
     }
 }
