28bca95e944d1d11a1feeb2c8d7f32fc3d470bfd
MKA1500 <maciejkabsch@gmail.com>
2018-09-08 20:48:06 +0200

    new component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {}
+  state = {
+    count: 0
+  };
+
   render() {
-    return ( )
+    return (
+      <div className="container mt-5">
+        <div>
+          <span>{ this.formatCount() }</span>
+        </div>
+        <button className="btn btn-success">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state; // i.e. = this.state.count
+    return count === 0 ? 'Zero' : count;
   }
 }
 
