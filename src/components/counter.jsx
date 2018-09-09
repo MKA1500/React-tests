6f643df10796a76feb02c7faf75c8a0f3533a059
MKA1500 <maciejkabsch@gmail.com>
2018-09-09 17:02:08 +0200

    styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,13 +5,20 @@ class Counter extends Component {
     count: 0
   };
 
+  styles= {
+    fonSize: '10px',
+    fontWeight: 'bold'
+  };
+
   render() {
     return (
       <div className="container mt-5">
         <div>
-          <span>{ this.formatCount() }</span>
+          <span
+            style={this.style}
+            className="badge badge-primary m-2">{ this.formatCount() }</span>
         </div>
-        <button className="btn btn-success">Increment</button>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
