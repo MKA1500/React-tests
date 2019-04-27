6dac9cc1d934660a57ad6f6a347b659c8c58fc99
MKA1500 <maciejkabsch@gmail.com>
2019-04-27 13:39:40 +0200

    some rehearsal...

--- new-example/src/App.js
+++ new-example/src/App.js
@@ -2,23 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function getLabel() {
+  return 'Learn React now';
+}
+
+const oneMore = { val: 1 };
+
 class App extends Component {
+  additionalText = 'hello';
+  secondMore = { val: 2 };
+  style = { backgroundColor: '#777', color: '#eee'};
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header
+          className="App-header"
+          style={ this.style }>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            { getLabel() }
           </a>
+          <p>{ this.additionalText }</p>
+          <p>{ oneMore.val } { this.secondMore.val }</p>
         </header>
       </div>
     );
