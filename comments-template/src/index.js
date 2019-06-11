bc9569a7e7a44b52ce507a13908c633dee8433aa
MKA1500 <maciejkabsch@gmail.com>
2019-06-11 20:20:16 +0200

    props excercises

--- comments-template/src/index.js
+++ comments-template/src/index.js
@@ -6,23 +6,34 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        authorName="Maciej"
-        timeAgo="Today at 4.50 PM"
-        comment="But it’s better to have a framework"/>
-      <CommentDetail
-        authorName="Darek"
-        timeAgo="Today at 4.13 PM"
-        comment="It’s possible" />
-      <CommentDetail
-        authorName="Asia"
-        timeAgo="Today at 3.59 PM"
-        comment="Is it possible to build microservices using Java EE?" />
-      <CommentDetail
-        authorName="Artur"
-        timeAgo="Today at 3.33 PM"
-        comment="microservices using Java EE..." />
+      <ApprovalCard>
+        <h2>Warning</h2>
+        <p>Are you sure you want to approve?</p>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          authorName="Maciej"
+          timeAgo="Today at 4.50 PM"
+          comment="But it’s better to have a framework"/>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          authorName="Darek"
+          timeAgo="Today at 4.13 PM"
+          comment="It’s possible" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          authorName="Asia"
+          timeAgo="Today at 3.59 PM"
+          comment="Is it possible to build microservices using Java EE?" />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          authorName="Artur"
+          timeAgo="Today at 3.33 PM"
+          comment="microservices using Java EE..." />
+      </ApprovalCard>
     </div>
   );
 };
