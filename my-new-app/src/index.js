3127e086dd3ecee097d36c08d105070ee6df5d8a
MKA1500 <maciejkabsch@gmail.com>
2021-07-04 19:52:38 +0200

    some exercises

--- my-new-app/src/index.js
+++ my-new-app/src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function getSecondBtnText() {
+    return 'Cancel';
+}
+
+const App = () => {
+    const buttonText = {
+        text: 'Submit the form'
+    };
+    const styleSecondBtn = {
+        backgroundColor: 'white',
+        color: 'black',
+        marginLeft: '5px'
+    };
+
+    return (
+    <div>
+        <label className="label" htmlFor="name">Enter name:</label>
+        <input id="name" type="text" />
+        <button style={{backgroundColor: 'blue', color: 'white'}}>
+            {buttonText.text}
+        </button>
+        <button style={styleSecondBtn}>
+            {getSecondBtnText()}
+        </button>
+    </div>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
