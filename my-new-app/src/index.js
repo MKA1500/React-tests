042f357f75e776a668f63c6cd567d777f6f8a4e0
MKA1500 <maciejkabsch@gmail.com>
2021-07-07 21:00:45 +0200

    props

--- my-new-app/src/index.js
+++ my-new-app/src/index.js
@@ -4,11 +4,27 @@ import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
+
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <CommentDetail
+                author="Maciej"
+                time="Today at 5:57 PM"
+                content="Brussels Region will have administered over one million doses of vaccines against Covid-19 by Sunday afternoon."
+                image={faker.image.avatar()} />
+            <CommentDetail
+                author="Gino"
+                time="Today at 6:00 PM"
+                content="Region will have administered over one million doses of vaccines against Covid-19 by Sunday."
+                image={faker.image.avatar()} />
+            <CommentDetail author="Monika"
+                time="Today at 5:43 PM"
+                content="Will have administered over one million doses of vaccines against Covid-19."
+                image={faker.image.avatar()} />
+            <CommentDetail author="Tom"
+                time="Today at 4:50 PM"
+                content="Administered over one million doses of vaccines"
+                image={faker.image.avatar()} />
         </div>
     );
 };
