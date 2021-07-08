682288e2be62d3c3cf2533bc56d48158dde66515
MKA1500 <maciejkabsch@gmail.com>
2021-07-08 08:47:12 +0200

    use of props.children

--- my-new-app/src/index.js
+++ my-new-app/src/index.js
@@ -2,29 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Maciej"
-                time="Today at 5:57 PM"
-                content="Brussels Region will have administered over one million doses of vaccines against Covid-19 by Sunday afternoon."
-                image={faker.image.avatar()} />
-            <CommentDetail
-                author="Gino"
-                time="Today at 6:00 PM"
-                content="Region will have administered over one million doses of vaccines against Covid-19 by Sunday."
-                image={faker.image.avatar()} />
-            <CommentDetail author="Monika"
-                time="Today at 5:43 PM"
-                content="Will have administered over one million doses of vaccines against Covid-19."
-                image={faker.image.avatar()} />
-            <CommentDetail author="Tom"
-                time="Today at 4:50 PM"
-                content="Administered over one million doses of vaccines"
-                image={faker.image.avatar()} />
+            <ApprovalCard>
+                <div>
+                    <h2>Warning</h2>
+                    Press 'Approve' to continue.
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Maciej"
+                    time="Today at 6:57 PM"
+                    content="Brussels Region will have administered over one million doses."
+                    image={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Gino"
+                    time="Today at 6:00 PM"
+                    content="Region will have administered over one million doses of vaccines against Covid-19 by Sunday."
+                    image={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Monika"
+                    time="Today at 5:43 PM"
+                    content="Will have administered over one million doses of vaccines against Covid-19."
+                    image={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 };
