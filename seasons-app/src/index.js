10cd66b540a132d94f25717b183e4746cd01dbd6
MKA1500 <maciejkabsch@gmail.com>
2021-07-27 21:22:33 +0200

    next step

--- seasons-app/src/index.js
+++ seasons-app/src/index.js
@@ -1,14 +1,17 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
-    );
-
-    return <div className="container">Hi there!</div>;
-};
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        );
+    
+        return <div className="container">Hi there!</div>;
+    };
+}
 
 ReactDOM.render(
     <App />,
