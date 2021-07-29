01e36826fae7f67871084d7e296b1debfd4a89e3
MKA1500 <maciejkabsch@gmail.com>
2021-07-29 20:52:57 +0200

    state

--- seasons-app/src/index.js
+++ seasons-app/src/index.js
@@ -3,13 +3,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: null,
+            long: null
+        };
+
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                this.setState({
+                    lat: position.coords.latitude,
+                    long: position.coords.longitude
+                });
+            },
             (error) => console.log(error)
         );
+    }
 
-        return <div className="container">Hello!</div>;
+    render() {
+        return <div className="container">
+            <h2 className="text-center">
+                Latitude: {this.state.lat}
+            </h2>
+            <h2 className="text-center">
+                Longitude: {this.state.long}
+            </h2>
+        </div>;
     };
 }
 
