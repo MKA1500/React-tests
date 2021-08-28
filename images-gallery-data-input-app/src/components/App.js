35802fee9b41ce0d45886d6d90ea9ea754de6d84
MKA1500 <maciejkabsch@gmail.com>
2021-08-28 21:09:38 +0200

    fetching data

--- images-gallery-data-input-app/src/components/App.js
+++ images-gallery-data-input-app/src/components/App.js
@@ -4,6 +4,13 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
     onSearchSubmit(term) {
         console.log(term);
+        // https://unsplash.com/documentation#search-photos
+        // https://unsplash.com/documentation#location 
+        fetch(`https://api.unsplash.com/search/photos?query=${term}`, {
+            headers: { Authorization: 'Client-ID xx' }
+        })
+        .then(response => response.json())
+        .then(data => console.log(data));
     }
 
     render() {
