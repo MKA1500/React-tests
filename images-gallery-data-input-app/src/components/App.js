23132b5d90a2968d0e34f2359b6c2a6ee2e3d72b
MKA1500 <maciejkabsch@gmail.com>
2021-09-11 16:16:51 +0200

    api response

--- images-gallery-data-input-app/src/components/App.js
+++ images-gallery-data-input-app/src/components/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term) {
+    cars = [];
+    onSearchSubmit = (term) => {
         console.log(term);
         // https://unsplash.com/documentation#search-photos
         // https://unsplash.com/documentation#location 
         fetch(`https://api.unsplash.com/search/photos?query=${term}`, {
-            headers: { Authorization: 'Client-ID xx' }
+            headers: { Authorization: 'Client-ID xxx' }
         })
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then((data) => {
+            if (data && data.results) {
+                this.cars = data.results;
+                console.log(this.cars);
+            }
+        });
     }
 
     render() {
