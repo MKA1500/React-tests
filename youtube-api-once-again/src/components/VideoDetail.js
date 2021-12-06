7320c36c05208fbd6a46cb7b655164610efa3f9d
MKA1500 <maciejkabsch@gmail.com>
2021-12-06 20:11:45 +0100

    video displayed in iframe

--- youtube-api-once-again/src/components/VideoDetail.js
+++ youtube-api-once-again/src/components/VideoDetail.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const VideoDetail = props => {
-    let title = props.video ? props.video.snippet.title : '';
+    let title = '';
+    let description = '';
+    let videoSrc = '';
+    if (props.video) {
+        title = props.video.snippet.title;
+        description = props.video.snippet.description;
+        videoSrc = `https://www.youtube.com/embed/${props.video.id.videoId}`;
+    }
 
-    return <div>{title}</div>;
+    return (
+        <div className="video-detail-container">
+            <div className="video-container">
+                <iframe width="100%" height="300" src={videoSrc} />
+            </div>
+            <h4>{title}</h4>
+            <p>{description}</p>
+        </div>
+    );
 }
 
 export default VideoDetail;
