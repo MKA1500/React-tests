0da060453541f247e78c905303370a96b4b81aec
MKA1500 <maciejkabsch@gmail.com>
2022-05-15 17:50:29 +0200

    redux new exercise

---
+++ redux-exercise-3/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+root.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  );
