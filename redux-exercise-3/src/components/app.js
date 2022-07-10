cd0b21aa645a166f440edf21237280f6b9ef5586
MKA1500 <maciejkabsch@gmail.com>
2022-07-10 20:41:57 +0200

    detail component

--- redux-exercise-3/src/components/app.js
+++ redux-exercise-3/src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BookList from '../containers/BookList';
+import BookDetail from '../containers/BookDetail';
 
 export default class App extends Component {
     render() {
@@ -8,6 +9,9 @@ export default class App extends Component {
                 <div className="row">
                     <div className="col-4">
                         <BookList />
+                    </div>
+                    <div className="col-8">
+                        <BookDetail />
                     </div>
                 </div>
             </div>      
