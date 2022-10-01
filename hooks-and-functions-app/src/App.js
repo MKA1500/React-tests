da3c1372b62dc7c01c092e3bff6fe8ae33155f4a
MKA1500 <maciejkabsch@gmail.com>
2022-10-01 19:46:59 +0200

    items moved to a function

--- hooks-and-functions-app/src/App.js
+++ hooks-and-functions-app/src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import Wikipedia from './components/Wikipedia';
 import Search from './components/Search';
+import GiveItems from './data/Items';
 import './App.css';
 
-const items = [
-    {
-        title: 'Lorem ipsum dolor sit amet?',
-        content: 'consectetur adipiscing elit. In vulputate mauris eget sem posuere, in ornare lorem pulvinar. Nulla id eros sit amet velit sollicitudin pharetra id a lectus.',
-        id: 1
-    },
-    {
-        title: 'Phasellus faucibus libero sed augue?',
-        content: 'Rhoncus placerat. Suspendisse tempus, ipsum sit amet aliquet ornare.',
-        id: 2
-    },
-    {
-        title: 'Turpis nisl aliquam orci?',
-        content: 'Eu aliquam enim nibh vel sem. Donec ipsum quam, lacinia a venenatis et, consequat ut orci.',
-        id: 3
-    }
-];
+const items = GiveItems();
 
 export default () => {
     return (
---
+++ hooks-and-functions-app/src/data/Items.js
@@ -0,0 +1,19 @@
+export default function GiveItems() {
+    return  [
+        {
+            title: 'Lorem ipsum dolor sit amet?',
+            content: 'consectetur adipiscing elit. In vulputate mauris eget sem posuere, in ornare lorem pulvinar. Nulla id eros sit amet velit sollicitudin pharetra id a lectus.',
+            id: 1
+        },
+        {
+            title: 'Phasellus faucibus libero sed augue?',
+            content: 'Rhoncus placerat. Suspendisse tempus, ipsum sit amet aliquet ornare.',
+            id: 2
+        },
+        {
+            title: 'Turpis nisl aliquam orci?',
+            content: 'Eu aliquam enim nibh vel sem. Donec ipsum quam, lacinia a venenatis et, consequat ut orci.',
+            id: 3
+        }
+    ];
+}
